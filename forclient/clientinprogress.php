22f266e99ac116738a1ac8696a4a842306d2124c
Paul Palo <kukishimoto17@gmail.com>
2024-08-25 11:38:13 +0800

    done

--- forclient/clientinprogress.php
+++ forclient/clientinprogress.php
@@ -10,7 +10,7 @@ if ($_SESSION['role'] != "Client2") {
 }
 
 $cidd = $_SESSION['idn'];
-include('Database.php'); // Ensure this file connects to your database
+include('Databases.php'); // Ensure this file connects to your database
 
 // Get the job_id from query parameter (if available)
 $job_id = isset($_GET['job_id']) ? $_GET['job_id'] : null;
@@ -142,4 +142,3 @@ $job_id = isset($_GET['job_id']) ? $_GET['job_id'] : null;
     </script>
 </body>
 </html>
-s
